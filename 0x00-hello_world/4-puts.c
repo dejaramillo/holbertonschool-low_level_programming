580651cc80cb486e2ec266b97929f81d419e0493
acer <dejaramillo@misena.edu.co>
2020-09-16 22:25:25 -0500

    [UPDATE]

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-* main - function puts
-* Return: 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{puts("\"Programming is like building  a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 return (0); }
