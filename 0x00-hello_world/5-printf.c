f5efa8374ce8846b5b5a0040045bbf57d3a67a28
acer <dejaramillo@misena.edu.co>
2020-09-16 21:47:54 -0500

    [ADD] printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/*
+*function pritnf for print string
+*/
+
+
+int main(void){
+
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+
+}
