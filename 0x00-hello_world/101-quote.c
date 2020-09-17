0b462a67380e204ef620e34221750212545561c9
acer <dejaramillo@misena.edu.co>
2020-09-17 10:55:36 -0500

    [ADD] Advance

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Write a C program that prints to the standard error.
+ *
+ * Return: and that piece of art is useful" - Dora Korpar, 2015-10-19
+ */
+
+int main(void)
+{write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 62);
+return (1); }
