e32a89daef5c185c2c9c9de5fb50b3cfef7bf45a
acer <dejaramillo@misena.edu.co>
2020-09-16 21:38:56 -0500

    [ADD] puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 /*
-*funtion puts
+*function puts
 */
 
-int main (void){
+int main(void)
+{
 
 
     puts("\"Programming is like building  a multilingual puzzle");
-    return(0);
+    return (0);
 
 }
-
-
-
