5280513f97f7a97c11596c0acdd036a3f991b0b4
acer <dejaramillo@misena.edu.co>
2020-09-18 11:18:29 -0500

    [ADD0 numers]

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print tthe numbers with putchar
+ *
+ * return: loops for
+ */
+
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
