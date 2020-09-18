f88662c0ad5f72345935cfeb8912103fe85594a3
acer <dejaramillo@misena.edu.co>
2020-09-18 10:34:50 -0500

    [ADD] number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the number cwith printf
+ *
+ * return: loop with number
+ */
+int main(void)
+{
+int numbers;
+while (numbers <= 9)
+{
+printf("%d", numbers);
+numbers++;
+}
+printf("\n");
+return (0);
+}
