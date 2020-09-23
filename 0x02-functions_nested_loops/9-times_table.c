f8da4839186f53bbc2065d7b7f457aab66556b76
acer <dejaramillo@misena.edu.co>
2020-09-23 16:53:18 -0500

    [ADD] times_tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * times_tables - print the number in list of 0 and 9
+ * Return: void
+ */
+
+void times_table(void)
+{
+int i, j, k;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+k = i * j;
+if (j == 0)
+_putchar((k % 10) + 48);
+else if ((k / 10) == 0 && j != 0)
+{
+_putchar(' ');
+_putchar((k % 10) + 48);
+}
+else
+{
+_putchar((k / 10) + 48);
+_putchar((k % 10) + 48);
+}
+if (j != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
