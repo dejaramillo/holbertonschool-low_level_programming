477679d233d0636e34f8c7d6b0b902924e11920c
acer <dejaramillo@misena.edu.co>
2020-09-24 22:42:20 -0500

    [ADD] more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * more_numbers - print numbers until fourteen
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+int n, x;
+for (n = 0; n <= 10; n++)
+{
+for (x = 0; x <= 14; x++)
+{
+if (x >= 10)
+{
+_putchar(48 + x / 10);
+_putchar(48 + x % 10);
+}
+else
+{
+_putchar(48 + x);
+}
+}
+_putchar('\n');
+}
+}
