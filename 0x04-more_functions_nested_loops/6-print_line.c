dc52f2e29fba57ac27c50a75dbb49cb484c59985
acer <dejaramillo@misena.edu.co>
2020-09-24 23:08:44 -0500

    [ADD]

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,12 @@
 
 void print_line(int n)
 {
-int init;
-for (init = 0; init <= n; init++)
+int x;
+if (n > 0)
 {
-if (n < 0)
+for (x = 0; x < n; x++)
 {
-_putchar('\n');
-break;
-}
-else
-{_putchar('_');
+_putchar('_');
 }
 }
 _putchar('\n');
