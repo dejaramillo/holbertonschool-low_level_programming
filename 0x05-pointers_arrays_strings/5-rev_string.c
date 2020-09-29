4a401f60fbca4a67f7b4213eab29ce79b8011b9a
acer <dejaramillo@misena.edu.co>
2020-09-29 10:51:25 -0500

    [ADD] string rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *rev_string - print string
+ *@s:entry point
+ *Return: void
+ */
+
+void rev_string(char *s)
+{
+int i, reverse;
+	char *lastchar, ch;
+	lastchar = s;
+	for (i = 0; *lastchar != '\0'; i++)
+	{
+		lastchar++;
+	}
+	lastchar = lastchar - 1;
+	for (reverse = 0; reverse < i / 2; reverse++)
+	{
+		ch = *lastchar;
+		*lastchar = *(s + reverse);
+		*(s + reverse) = ch;
+		lastchar--;
+	}
+}
