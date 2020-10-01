1a6d779553cb102082ad18d7f6323fdd0a3cf0f5
acer <dejaramillo@misena.edu.co>
2020-10-01 15:58:29 -0500

    [ADD] uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * *string_toupper - print string uppercase
+ *@ch: entry point
+ *Return: char
+ */
+
+char *string_toupper(char *ch)
+{
+	int i;
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		if (ch[i] >= 'a' && ch[i] <= 'z')
+		{
+			ch[i] -= 32;	
+		}
+	}
+	return (ch);
+}
