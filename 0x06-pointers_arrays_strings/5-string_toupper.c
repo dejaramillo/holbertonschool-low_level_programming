120824eb136e3b3bcd028a76f72856f305e0328e
acer <dejaramillo@misena.edu.co>
2020-10-01 15:59:26 -0500

    [ADD] uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,12 +7,12 @@
 
 char *string_toupper(char *ch)
 {
-	int i;
+int i;
 	for (i = 0; ch[i] != '\0'; i++)
 	{
 		if (ch[i] >= 'a' && ch[i] <= 'z')
 		{
-			ch[i] -= 32;	
+			ch[i] -= 32;
 		}
 	}
 	return (ch);
