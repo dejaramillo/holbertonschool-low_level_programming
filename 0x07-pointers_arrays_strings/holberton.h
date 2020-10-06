06dc8d29a04e582fb8bc25df35618338d6b080c4
acer <dejaramillo@misena.edu.co>
2020-10-06 18:41:32 -0500

    [ADD] chessboard

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,13 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+int _putchar(char c);
 char *_memset(char *s, char b, int n);
 char *_memcpy(char *dest, char *src, unsigned n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+void print_chessboard(char (*a)[8]);
 
 
 #endif
