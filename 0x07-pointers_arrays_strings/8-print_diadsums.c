0377cb507d83f0e79599c76ea1b6a3f91c1adef1
acer <dejaramillo@misena.edu.co>
2020-10-06 19:31:04 -0500

    [ADD] diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diadsums.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_diagsums - Function that prints the sum of the two diagonals of matrix
+ *@a: first entry point
+ *@size: second entry point
+ *Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+int i, result1, result2;
+	for (i = 0; i < (size * size); i++)
+	{
+		if (i % (size + 1) == 0)
+		{
+			result1 += (a + i);
+		}
+
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+		{
+			result2 += (a + i);
+		}
+	}
+	printf("%ld, %ld\n", sum1, sum2);
+}
