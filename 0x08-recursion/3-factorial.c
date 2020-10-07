305599f6f507db84038d886978b6c3e909e5ff32
acer <dejaramillo@misena.edu.co>
2020-10-07 13:18:09 -0500

    [ADD] factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *factorial - print factorial of a number
+ *@n:entry point
+ *Return: int
+ */
+
+int factorial(int n)
+{
+	if ( n <= 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n -1));
+	}
+}
