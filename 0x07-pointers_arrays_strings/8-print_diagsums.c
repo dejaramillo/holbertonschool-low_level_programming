418005c83aeb0ad7469d801b4d3dbb7c63465cc1
acer <dejaramillo@misena.edu.co>
2020-10-06 19:46:36 -0500

    [ADD] dia

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,18 +9,15 @@
 
 void print_diagsums(int *a, int size)
 {
-long int i, result1, result2;
+	long int i, sum1 = 0, sum2 = 0;
+
 	for (i = 0; i < (size * size); i++)
 	{
 		if (i % (size + 1) == 0)
-		{
-			result1 += *(a + i);
-		}
-
+			sum1 += *(a + i);
 		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-		{
-			result2 += *(a + i);
-		}
+			sum2 += *(a + i);
 	}
-	printf("%ld, %ld\n", result1, result2);
+
+	printf("%ld, %ld\n", sum1, sum2);
 }
