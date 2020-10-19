f7eaeb745d9aa0db18f5e1c78c9e585824c1f01d
acer <dejaramillo@misena.edu.co>
2020-10-19 18:45:03 -0500

    [ADD] create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,13 +10,13 @@
 
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
-	char *s;
+unsigned int i;
+char *s;
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	s = malloc(size *sizeof(char));
+	s = malloc(size * sizeof(char));
 
 	if (s == NULL)
 	{
