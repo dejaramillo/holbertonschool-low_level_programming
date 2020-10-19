94030f9184eaeb8f5eb3b55007666f5995d7a5c5
acer <dejaramillo@misena.edu.co>
2020-10-19 12:02:38 -0500

    [ADD] myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
  *main - print name
+ *@argc: Number of arguments passed
+ *@argv: Array of strings
  *Return: 0(success)
  */
-
+sy
 int main(int argc, char *argv[])
 {
-	printf ("%s\n", argv[argc - 1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
