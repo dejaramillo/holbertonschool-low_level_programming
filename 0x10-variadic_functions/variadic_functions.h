e7f0824423f2704c884551811fab92d7cf3424f1
acer <dejaramillo@misena.edu.co>
2020-10-24 12:07:27 -0500

    [ADD] _putchar

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,7 @@ typedef struct form
 	void (*f)(va_list);
 } fr;
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
